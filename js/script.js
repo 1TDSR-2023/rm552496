496f8adf2e92922aa1c10c90fe7b4a78ac3cc6ef
Caio Boris <caio.correa@neomode.com.br>
2023-05-03 23:10:35 -0300

    aula js -> getElementById e addEventListener

--- js/script.js
+++ js/script.js
@@ -1,10 +1,26 @@
-var nome1 = "Petroni";
-let nome2 = "Derick";
-const nome3 = "Ruan";
+// var nome1 = "Petroni";
+// let nome2 = "Derick";
+// const nome3 = "Ruan";
 
-if(nome1 != ""){
-    let nome1 = "Geovana"
-}
+// if(nome1 != ""){
+//     let nome1 = "Geovana"
+// }
 
-console.log(nome1)
+// console.log(nome1)
 
+let botao = document.getElementById("meu-btn");
+botao.addEventListener("click", function(){
+    //Funçao matemática Math
+    //random = retorna um número aleatório entre 0 e 1
+    //floor = arredonda um numero para baixo
+    //ceil = arrendonda um numero para cima
+    //round = arredonda um número aleatóriamente
+    let r=0, g=0, b=0;
+    
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+
+    this.setAttribute("style", `background-color: rgb(${r},${g},${b});`)
+
+});
