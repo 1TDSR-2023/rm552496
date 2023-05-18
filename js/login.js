edc8fbae969de670fce507941e6ad7cb4c594349
Caio Boris <caio.correa@neomode.com.br>
2023-05-17 23:00:17 -0300

    Aula validação de form

--- js/login.js
+++ js/login.js
@@ -43,3 +43,103 @@ eyePass.addEventListener("click", () =>{
         eyePass.setAttribute("class", "fa fa-eye")
     }
 });
+
+
+
+
+// //Objeto
+// const usuario1 = {
+//     nomeCompleto : "José das Couves",
+//     emailUsuario: "lucas@email.com",
+//     senhaUsuario: "123456"
+
+// }
+
+// const usuario2 = {
+//     nomeCompleto : "Juan Pelego",
+//     emailUsuario: "juan@email.com",
+//     senhaUsuario: "123456"
+
+// }
+
+//Lista de usuários
+let listaDeUsuarios = [
+    {
+        nomeCompleto: "José das Couves",
+        emailUsuario: "lucas@email.com",
+        senhaUsuario: "123456"
+    },
+    {
+        nomeCompleto: "Juan Pelego",
+        emailUsuario: "juan@email.com",
+        senhaUsuario: "123456"
+    },
+    {
+        nomeCompleto: "Caio Tulipa",
+        emailUsuario: "tulipa@email.com",
+        senhaUsuario: "123456"
+    },
+    {
+        nomeCompleto: "Derick Gouvea",
+        emailUsuario: "dede@email.com",
+        senhaUsuario: "123456"
+    },
+    {
+        nomeCompleto: "Matheuzim Danado",
+        emailUsuario: "mateu@email.com",
+        senhaUsuario: "123456"
+    },
+    {
+        nomeCompleto: "Leunardo Camacho",
+        emailUsuario: "lelu@email.com",
+        senhaUsuario: "123456"
+    }
+];
+
+
+const botaoSubmit = document.querySelector("#btnSubmit");
+
+botaoSubmit.addEventListener("click", ()=>{
+    
+    //Criando um objeto de validação
+    let userValidado = {
+        emailUsuarioValidado : inputEmail.value,
+        senhaUsuarioValidado : inputPass.value
+    }
+    
+    //Criando uma iteração com a lista de objetos para realizar a validação
+    // com o usuarioValidado
+    try{
+        listaDeUsuarios.forEach((usuario) =>{
+            //Realizando a validaçao
+            //Comparando cada objeto da lista com o objeto montado com os dados 
+            //que vieram do formulario
+
+            if(userValidado.emailUsuarioValidado == usuario.emailUsuario && userValidado.senhaUsuarioValidado == usuario.
+                senhaUsuario){
+                    userValidado = usuario;
+                    throw "VALIDADO";
+            }
+        });
+
+        throw "SENHA OU EMAIL INVALIDO";
+        
+    }catch(msg){
+
+        const msgStatus = document.querySelector("#msgStatus");
+
+        if(msg == "VALIDADO"){
+            msgStatus.setAttribute("style", "color: #00ff00");
+            msgStatus.innerHTML = `<span><strong>Usuário: ${userValidado.nomeCompleto} Login efetuado com sucesso!</strong></span>`
+
+            //Redirect
+            setTimeout(() =>{
+                window.location.href = "../home.html"
+            }, 3000);
+        }else{
+            msgStatus.setAttribute("style", "color: #ff0000");
+            msgStatus.innerHTML = "<span><strong>Email ou senha inválidos!</strong></span>"
+        }
+    }
+
+});
